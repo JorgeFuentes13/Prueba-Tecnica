c72f483d3a3b3de730b7893ea438d9ed05598905
JorgeFuentes13 <jo.fueness@duocuc.cl>
2025-01-08 06:01:37 -0300

    Zoom marcadores

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import {GoogleMap, MapAdvancedMarker} from '@angular/google-maps';
 import { DirectionFormComponent } from '../components/direction-form/direction-form.component';
 import { MarkerService } from '../services/marker.service';
@@ -13,6 +13,7 @@ import { Subscription } from 'rxjs';
 })
 export class MapComponent implements OnInit  {
 
+  @ViewChild('map') googleMap!: GoogleMap;
 
   private markerService = inject(MarkerService)
   public MarkerSus: Subscription = Subscription.EMPTY;
@@ -20,11 +21,11 @@ export class MapComponent implements OnInit  {
   advancedMarkerOptions: google.maps.marker.AdvancedMarkerElementOptions = {gmpDraggable: false};
   advancedMarkerPositions: google.maps.LatLngLiteral[] = [];
 
-
   ngOnInit(): void {
     this.MarkerSus = this.markerService.getGeoDataMarker().subscribe((markerData: any) => {
       console.log('Received marker data: ', markerData);
       this.addMarker(markerData);
+      this.adjustMapToMarkers();
     });
   }
 
@@ -33,20 +34,28 @@ export class MapComponent implements OnInit  {
   }
 
   center: google.maps.LatLngLiteral = { lat: -33.45694, lng: -70.64827};
-  zoom = 11;
+  zoom = 13;
 
   mapOptions: google.maps.MapOptions = {
     mapTypeControl: false,
     streetViewControl: false,
   };
 
+
   addMarker(markerData: { lat: number; lng: number }): void {
     this.advancedMarkerPositions.push({
       lat: markerData.lat,
       lng: markerData.lng,
     });
-    console.log('marquer creado')
   }
 
-
+  adjustMapToMarkers(): void {
+    const bounds = new google.maps.LatLngBounds();
+    this.advancedMarkerPositions.forEach((position) => {
+      bounds.extend(position);
+    });
+    if (this.googleMap) {
+      this.googleMap.fitBounds(bounds);
+    }
+  }
 }
